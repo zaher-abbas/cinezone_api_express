095a665bc524dd58611a57ceca4b79ce2e30ceae
Zaher ABBAS <z.abbas83@yahoo.com>
2025-08-06 11:41:05 +0200

    Add environment variable support and root endpoint:
    - Integrate `dotenv` for configuration management.
    - Add `PORT` variable fallback in server initialization.
    - Create `/` endpoint for API welcome message.
    - Update `.env.example` with `PORT` placeholder.

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 import {movies, movieDetail, categoryMovies} from './MovieController.js';
 
 const app = express();
 
-const listeningPort = 3001
-
 app.use(express.json());
 
+const listeningPort = process.env.PORT || 3000; //3000 c'est la valeur de secours au cas ou env.PORT n'existe pas
+
 app.listen(listeningPort, () => console.log(`Server is listening on port ${listeningPort}`));
+
+//Home endpoint / Root endpoint
+app.get('/', (req, res) => {
+    res.send('Welcome to CineZone API')
+})
 
 //Get with 'limit' and 'category' queries
 app.get('/movies', movies)

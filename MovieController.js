3f62befd82601e42c4f1b78476119f80afe1b55e
Zaher ABBAS <z.abbas83@yahoo.com>
2025-08-06 10:18:18 +0200

    Optimize movie queries in `movies` method with direct SQL filtering and pagination logic adjustments.

--- MovieController.js
+++ MovieController.js
@@ -1,30 +1,49 @@
 import {database} from "./database.js";
 
-
 export const movies = (req, res) => {
-    database.query('SELECT * FROM movie')
-        .then(result => {
-            const [movies] = result; //on a déconstructuré le tableau result par [] et affecté le 1er element à books
-            const limit = req.query.limit
-            const minRating = req.query.min_rating
-            if (minRating && !limit) {
-                const minRatedMovies = movies.filter(movie => movie.rating >= minRating);
-                return res.json(minRatedMovies);
-            } else if (minRating && limit) {
-                const minRatedMovies = movies.filter(movie => movie.rating >= minRating);
-                const filteredMovies = minRatedMovies.slice(0, limit);
-                return res.json(filteredMovies);
-            } else if (!minRating && limit) {
-                const filteredMovies = movies.slice(0, limit);
-                return res.json(filteredMovies);
-            }
-            return res.json(movies);
-        })
-        .catch(err => {
-            console.error(err)
-        })
-}
+    const limit = parseInt(req.query.limit)
+    const minRating = parseFloat(req.query.min_rating)
 
+    if (limit && limit > 0 && !minRating) {
+        database.query('SELECT * FROM movie LIMIT ?', [limit])
+            .then(result => {
+                const [movies] = result;
+                return res.json(movies);
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    } else if (limit && limit > 0 && minRating) {
+        database.query('SELECT * FROM movie WHERE rating >= ? LIMIT ?', [minRating, limit])
+            .then(result => {
+                const [movies] = result;
+                return res.json(movies);
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    } else if (!limit && minRating) {
+        database.query('SELECT * FROM movie WHERE rating >= ?', [minRating])
+            .then(result => {
+                const [movies] = result;
+                return res.json(movies);
+            })
+            .catch(err => {
+                console.error(err)
+            })
+
+
+    } else {
+        database.query('SELECT * FROM movie')
+            .then(result => {
+                const [movies] = result;
+                return res.json(movies);
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    }
+}
 export const movieDetail = (req, res) => {
     const id = req.params.id;
     database.query('SELECT * FROM movie WHERE id = ?', [id])

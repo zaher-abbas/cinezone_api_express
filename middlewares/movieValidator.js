7ff8f6fbc85d132c91302a2ed79c6ed3257f361d
Zaher ABBAS <z.abbas83@yahoo.com>
2025-08-07 17:28:01 +0200

    Trim and enforce whitespace validation in movieValidator inputs.

--- middlewares/movieValidator.js
+++ middlewares/movieValidator.js
@@ -2,15 +2,18 @@ import {body} from 'express-validator'
 import {handleValidationErrors} from "./handleValidationErrors.js";
 
 export const movieValidator = [
-    body('title').notEmpty().withMessage('Movie title is required')
+    body('title').trim()
+        .notEmpty().withMessage('Movie title is required')
         .isLength({max: 100}).withMessage('Movie title must not exceed 100 characters long'),
-    body('director').notEmpty().withMessage('Movie director is required')
+    body('director').trim()
+        .notEmpty().withMessage('Movie director is required')
         .isLength({max: 100}).withMessage('Movie director must not exceed 100 characters long'),
-    body('release_year').notEmpty().withMessage('Release year is required')
+    body('release_year').trim()
+        .notEmpty().withMessage('Release year is required')
         .isInt({min: 1900}).withMessage('Year must be a integer and greater than 1900')
-        .isLength({max: 4}).withMessage('Year must be 4 digits long')
     ,
-    body('rating').notEmpty().withMessage('Rating is required')
+    body('rating').trim()
+        .notEmpty().withMessage('Rating is required')
         .isFloat({min: 1, max: 10}).withMessage('Rating must be a float between 1 and 10'),
     handleValidationErrors
 
